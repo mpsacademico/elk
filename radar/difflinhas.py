c749c8a0ef9d1cfcd6bb95495d7386ecd99d132c
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-07-13 14:50:20 -0300

    Melhorias no nomenclatura das variáveis do difflinhas

--- radar/difflinhas.py
+++ radar/difflinhas.py
@@ -1,33 +1,36 @@
 import os
-oi=[]
-oc = {}
-for filename in os.listdir("."):
-    if filename.endswith(".Txt"):
-        #print(os.path.join(".", file))	
+
+fl = [] #[filename, line] - fl
+lf = {} #{line: filenames} - lf
+
+diretorio = "."
+extensao = ".Txt"
+
+for filename in os.listdir(diretorio):
+    if filename.endswith(extensao):        
 	f = open(filename, "r")
 	c = 0
 	for line in f:		
-		if c ==1: # 0 para primeira linha e 1 para segunda linha
-			oi.append([filename, line])
-		c=c+1
-		if c==2:
+		if c == 1: # 0 - primeira linha e 1 - segunda linha
+			fl.append([filename, line])
+		c = c + 1
+		if c == 2:
 			break
 	f.close()
 
-for o in oi:
-	if o[1] in oc:
-		oc[o[1]].append(o[0])
+for x in fl:
+	if x[1] in lf:
+		lf[x[1]].append(x[0])
 	else:
-		oc[o[1]] = [o[0]]
+		lf[x[1]] = [x[0]]
 
-print("qtd tipos de cabecalho:")
-print(len(oc.keys()))
-
+print("DIFFLINHAS v1.0.0.0 - Marcos Paulo da Silva")
+print("%s %s - %d linhas"%(diretorio, extensao, len(lf.keys())))
 print("\n\n---------------------------------------------------\n\n")
 
-for chave, valor in oc.items():
-	print(chave)
-	print(len(valor))
-	for v in valor:		
+for key, value in lf.items():
+	print(key)
+	print("%d arquivos\n"%(len(value)))
+	for v in value:		
 		print(v)
 	print("\n\n---------------------------------------------------\n\n")
